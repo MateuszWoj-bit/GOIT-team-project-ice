a2c7e7195f866f3acb691bcf3aa3fa575b20dc77
MateuszWoj-bit <79790198+MateuszWoj-bit@users.noreply.github.com>
2022-09-02 09:13:32 +0000

    Deploying to gh-pages from @ MateuszWoj-bit/GOIT-team-project-ice@9419742cb6c9a0ca4c48672122b34bb30fd463a3 🚀

--- src.69ad303f.js
+++ src.69ad303f.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/GOIT-team-project-test/src.a790c845.js.map
+//# sourceMappingURL=/GOIT-team-project-ice/src.69ad303f.js.map
